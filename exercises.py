f276aa61e31ec022f52d4481b474ea9851b48ba2
lightningrock <29sxiao@gmail.com>
2021-05-06 09:51:31 -0400

    Add exercises

---
+++ exercises.py
@@ -0,0 +1,18 @@
+##===============================Introduction to Programming Exercises===============================##
+
+
+# Exercise 1: Mailing Address
+'print("Sean X.\nNone\nNone\n2 Upper Woods Cir.\nNew Harford, New York\nUnited States of America")'
+
+# Exercise 2: Hello
+'print("Hi,", input("What is your name? "), "!")'
+
+# Exercise 3: Area of a Room
+'print("The length of your room is", float(input("Width of your room? "))*float(input("Length of your room? ")), "meters!")'
+
+# Exercise 4: Area of a Field
+print("How many acres your field has?", (int(input("Length of field? "))*int(input("Width of field? ")))/43560)
+
+
+
+##===============================Recursion Exercises===============================##
